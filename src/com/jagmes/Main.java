66945a9e4d54f6c39a103c635a5448cd944c49dd
jagmes <Gems8867@gmail.com>
2023-09-25 17:44:38 +0300

    Домашний калькулятор v2

--- src/com/jagmes/Main.java
+++ src/com/jagmes/Main.java
@@ -2,6 +2,7 @@
 
 package com.jagmes;
 
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
@@ -9,22 +10,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner insert = new Scanner(System.in);
         System.out.println("Вас приветстует калькулятор Т-1000.");
-        System.out.println("Введте первое целое число.");
-        int x = insert.nextInt();
-        System.out.println("Введте второе целое число.");
-        int y = insert.nextInt();
-        //складываем
-          int sum = x+y;
-          // вычетаем
-         int z= x-y;
-         // multyply
-        int m =x*y;
-        // делим
-        int d = x/y;
-        System.out.println("Результаты операций на числами:");
-        System.out.printf("Сложение: %d + %d = %d", x, y , sum);
-        System.out.printf("\n Вычетание: %d - %d = %d", x, y , z);
-        System.out.printf("\n Умножение: %d * %d = %d", x, y , m);
-        System.out.printf("\n Деление: %d / %d = %d", x, y , d);
+        System.out.println("Вы хотите воспользоваться калькулятором? 1-д, 2-н");
+         int ans = insert.nextInt();
+        if (ans == 1) {
+            System.out.println("Введите первое целое число.");
+            int x = insert.nextInt();
+            System.out.println("Введите второе целое число.");
+            int y = insert.nextInt();
+            System.out.println("выберите операцию: 1: +, 2: -, 3: *, 4: /.");
+            char operation = (char) insert.nextInt();
+            switch (operation) {
+                case 1:
+                    int sum = x + y;
+                    System.out.printf("Сложение: %d + %d = %d", x, y, sum);
+                    break;
+                case 2:
+                    int z = x - y;
+                    System.out.printf(" Вычетание: %d - %d = %d", x, y, z);
+                    break;
+                case 3:
+                    int m = x * y;
+                    System.out.printf(" Умножение: %d * %d = %d", x, y, m);
+                    break;
+                case 4:
+                    int d = x / y;
+                    System.out.printf(" Деление: %d / %d = %d", x, y, d);
+                    break;
+                default:
+                    return;
+            }
+        }
+        else  {
+            System.exit(0);
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
